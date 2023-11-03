5d6063917bfa4624e9d6c0eed0c07d9a002ebcc1
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-11-03 19:11:52 -0300

    Servers had no money lol

--- servers/home/hack.ts
+++ servers/home/hack.ts
@@ -2,9 +2,13 @@ export async function main(ns: NS) {
 	const server: string = <string> ns.args[0]
 	while (true) {
 		// Guide https://darktechnomancer.github.io/#glossary-of-terms
-		await ns.weaken(server)
-		await ns.grow(server)
-		await ns.weaken(server)
-		await ns.hack(server)
+
+		if (ns.getServerSecurityLevel(server) > 5) {
+			await ns.weaken(server)
+		} else if (ns.getServerMoneyAvailable(server) < ns.getServerMaxMoney(server) * 0.95) {
+			await ns.grow(server)
+		} else {
+			await ns.hack(server)
+		}
 	}
 }
