536eebed56ac5a627364ec93f6bca4138ee5a889
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-11-03 16:52:15 -0300

    Remove test script

--- servers/home/ramcost.ts
+++
@@ -1,6 +0,0 @@
-import {NS} from "NetscriptDefinitions";
-import {executeScriptOnServerFromAnother} from "./utils";
-
-export async function main(ns: NS) {
-	executeScriptOnServerFromAnother(ns, "home", "servers/home/ramcost.ts", 1, [])
-}
