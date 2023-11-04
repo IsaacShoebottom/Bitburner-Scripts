1af46c5d6fcd0dddcc952f6077dc6847c5290e21
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-11-04 17:14:34 -0300

    Fix hack all bug

--- servers/home/hackallservers.ts
+++ servers/home/hackallservers.ts
@@ -6,6 +6,7 @@ export async function main(ns: NS) {
 	for (const server of servers) {
 		let numThreads = ns.getServerMaxRam(server) / ns.getScriptRam("hack.js")
 		numThreads = Math.floor(numThreads)
+		numThreads = Math.max(numThreads, 1) // Make sure we have at least 1 thread
 		executeScriptOnServerFromAnother(ns, server, "hack.js", numThreads, [server])
 	}
 }
