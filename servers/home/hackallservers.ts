f0d401771120562c850273d0e64b4391df317849
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-11-13 13:26:43 -0400

    Add potential guard clause

--- servers/home/hackallservers.ts
+++ servers/home/hackallservers.ts
@@ -4,6 +4,9 @@ export async function main(ns: NS) {
 	let servers: string[] = recursiveScan(ns)
 
 	for (const server of servers) {
+		// Guard clause for story servers, but hacking xp is nice. Can probably be enabled when XP is easy to get
+		// if (ns.getServerMaxMoney(server) === 0) { continue }
+
 		let numThreads = ns.getServerMaxRam(server) / ns.getScriptRam("hack.js")
 		numThreads = Math.floor(numThreads)
 		numThreads = Math.max(numThreads, 1) // Make sure we have at least 1 thread
